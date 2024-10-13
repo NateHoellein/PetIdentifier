7af2543447f9d8691f50345e230131c1a2cb67ac
Nate <natehoellein@gmail.com>
2024-10-13 11:32:43 -0400

    Detecting breeds dogs / cats

--- PetIdentifier/DetectionViewModel.swift
+++ PetIdentifier/DetectionViewModel.swift
@@ -12,6 +12,8 @@ class DetectionViewModel: ObservableObject {
     @Published var breed: String?
     @Published var accuracy: String?
     
+    private let classifier = PetBreeds()
+    
     func reset() {
         DispatchQueue.main.async {
             self.image = nil
@@ -21,5 +23,25 @@ class DetectionViewModel: ObservableObject {
     }
     
     func classifyImage() {
+        if let image = self.image {
+            let resizedImage = resizeImage(image)
+            DispatchQueue.global(qos: .userInteractive).async {
+                self.classifier.classify(image: resizedImage ?? image) { [weak self] breed, confidence in
+                    DispatchQueue.main.async {
+                        self?.breed = breed ?? "Unknown"
+                        self?.accuracy = String(format: "%.2f%%", (confidence ?? 0) * 100.0)
+                    }
+                }
+            }
+        }
+    }
+    
+    private func resizeImage(_ image: UIImage) -> UIImage? {
+        let size = CGSize(width: 224, height: 224)
+        UIGraphicsBeginImageContextWithOptions(size, false, 0.0)
+        image.draw(in: CGRect(x: 0, y: 0, width: size.width, height: size.height))
+        let resizedImage = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        return resizedImage
     }
 }
