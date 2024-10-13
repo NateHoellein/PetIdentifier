bef464ad8d93d47908364d8aa3559e7a0a8fcb62
Nate <natehoellein@gmail.com>
2024-10-13 08:27:37 -0400

    Initial Commit

---
+++ PetIdentifier/PetIdentifierApp.swift
@@ -0,0 +1,17 @@
+//
+//  PetIdentifierApp.swift
+//  PetIdentifier
+//
+//  Created by nate on 10/13/24.
+//
+
+import SwiftUI
+
+@main
+struct PetIdentifierApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
